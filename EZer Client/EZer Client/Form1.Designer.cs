169ae20405400d438a8e2ef7ddce9db8040b3d58
gnmmarechal <gnmpolicemata91@gmail.com>
2016-05-03 23:58:51 +0100

    Added Goomba and Goomba Color Emulators

--- EZer Client/EZer Client/Form1.Designer.cs
+++ EZer Client/EZer Client/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 372);
+            this.ClientSize = new System.Drawing.Size(665, 292);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "EZer Client  for EZ-Flash IV";
             this.ResumeLayout(false);
 
         }
